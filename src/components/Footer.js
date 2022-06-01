e74ed3ed708a79fed1a5a71506ef0551f272856b
millennialmoron <millennialmoron87@gmail.com>
2022-06-01 16:41:42 +0200

    part two commit

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+export function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
+}
---
+++ src/components/Note.js
@@ -0,0 +1,8 @@
+export function Note(props) {
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+    </div>
+  );
+}
